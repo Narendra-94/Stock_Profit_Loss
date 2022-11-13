a025b8e54dc6c94729286acd41fe61c6dca5f8df
NiyaRakhewar <117759505+naren-07@users.noreply.github.com>
2022-11-14 00:21:58 +0530

    update

---
+++ index.js
@@ -0,0 +1,35 @@
+var initialPrice = document.querySelector("#initial");
+var stockQuantity = document.querySelector("#quantity");
+var currentPrice = document.querySelector("#current");
+var submit = document.querySelector("#submit");
+var output = document.querySelector("#output");
+
+submit.addEventListener('click', submitHandler);
+
+function submitHandler(){
+    var ip = Number(initialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var cur = Number(currentPrice.value);
+
+    calculate(ip, qty, cur);
+}
+
+function calculate(initial, quantity, current){
+    if(initial > current){
+        var loss = (initial - current) * quantity;
+        var lossPer = (loss/initial) * 100;
+
+        outputShow('loss is ' + loss + ' and percentage '+ lossPer + ' %');
+    } else if(current > initial){
+        var profit = (current - initial) * quantity;
+        var profitPer = (profit/initial) * 100;
+
+        outputShow('profit is ' + profit + ' and percentage '+ profitPer + ' %');
+
+    } else{
+        outputShow("No pain, No gain");
+    }
+}
+function outputShow(message){
+    output.innerHTML = message;
+}
