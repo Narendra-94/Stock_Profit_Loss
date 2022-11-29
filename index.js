b5c5bc895af4633ec7f321d57c04879cfacfe562
Narendra <narenchordiya07@gmail.com>
2022-11-30 01:44:41 +0530

    update: Wrote a condition for invalid number

--- index.js
+++ index.js
@@ -4,32 +4,35 @@ var currentPrice = document.querySelector("#current");
 var submit = document.querySelector("#submit");
 var output = document.querySelector("#output");
 
-submit.addEventListener('click', submitHandler);
+submit.addEventListener("click", submitHandler);
 
-function submitHandler(){
-    var ip = Number(initialPrice.value);
-    var qty = Number(stockQuantity.value);
-    var cur = Number(currentPrice.value);
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stockQuantity.value);
+  var cur = Number(currentPrice.value);
 
-    calculate(ip, qty, cur);
+  calculate(ip, qty, cur);
 }
 
-function calculate(initial, quantity, current){
-    if(initial > current){
-        var loss = (initial - current) * quantity;
-        var lossPer = (loss/initial) * 100;
+function calculate(initial, quantity, current) {
+  if (quantity >= 0) {
+    if (initial > current) {
+      var loss = (initial - current) * quantity;
+      var lossPer = (loss / initial) * 100;
 
-        outputShow('loss is ' + loss + ' and percentage '+ lossPer + ' %');
-    } else if(current > initial){
-        var profit = (current - initial) * quantity;
-        var profitPer = (profit/initial) * 100;
+      outputShow("loss is " + loss + " and percentage " + lossPer + " %");
+    } else if (current > initial) {
+      var profit = (current - initial) * quantity;
+      var profitPer = (profit / initial) * 100;
 
-        outputShow('profit is ' + profit + ' and percentage '+ profitPer + ' %');
-
-    } else{
-        outputShow("No pain, No gain");
+      outputShow("profit is " + profit + " and percentage " + profitPer + " %");
+    } else {
+      outputShow("No pain, No gain");
     }
+  } else {
+    outputShow("Invalid Stock Number");
+  }
 }
-function outputShow(message){
-    output.innerHTML = message;
+function outputShow(message) {
+  output.innerHTML = message;
 }
